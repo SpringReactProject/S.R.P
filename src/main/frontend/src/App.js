7b3df73526b2e9724a2d16c7cbb78b0a8b581823
noname_dev <noname_dev@naver.com>
2024-03-22 12:57:44 +0900

    Proxy Setting

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,7 +3,21 @@ import Home from "./Page/Home";
 import Login from "./Components/Login";
 import Navigation from "./Components/Navigation";
 import { Route, Routes } from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 function App() {
+
+    const [hello, setHello] = useState('');
+
+    useEffect(() => {
+        axios.get('/api/test')
+            .then((res) => {
+                setHello(res.data);
+            })
+    }, []);
+
+
   return (
     <div className="App">
       <Navigation />
@@ -11,6 +25,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
       </Routes>
+
+        백엔드 데이터 : {hello}
     </div>
   );
 }
